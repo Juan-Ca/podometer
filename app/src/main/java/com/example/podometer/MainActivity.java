5d4997d851fd9c78610d812967dcf20ef272134b
konfiot <arthur.toussaint@polytechnique.edu>
2019-10-03 10:33:26 -0400

    Première tentative compte pas

--- app/src/main/java/com/example/podometer/MainActivity.java
+++ app/src/main/java/com/example/podometer/MainActivity.java
@@ -2,13 +2,50 @@ package com.example.podometer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private SensorManager sensorManager;
+    private Sensor sensor;
+    private int initial_count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_FASTEST);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        Sensor sensor = sensorEvent.sensor;
+
+        if (sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
+            Log.i("History", "Data point:" + sensorEvent.values[0]);
+            //map.putDouble("steps", sensorEvent.values[0]);
+            if (initial_count == 0) {
+                initial_count = (int) sensorEvent.values[0];
+            } else {
+                TextView myAwesomeTextView = (TextView)findViewById(R.id.counts);
+                myAwesomeTextView.setText((new Integer((int) sensorEvent.values[0])).toString());
+            }
+        }
+    }
+
+    @Override
+    public final void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Do something here if sensor accuracy changes.
+    }
+
 }
